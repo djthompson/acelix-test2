b3ca19dea40792c547d3f51d75af443825298a11
Dan <dthompson7@gmail.com>
2013-06-29 20:31:02 +0000

    Inital Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AcelixTest2::Application.config.secret_key_base = 'ddc0b2b1dd3c08e452762f0f59669981d341a8adfaca18bebd611fe28b1de81f7c3c95c42509e9742bf0001344fa5951cabbf4a7d25e231ad62c68d978512039'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AcelixTest2::Application.config.session_store :cookie_store, key: '_acelix-test2_session'
