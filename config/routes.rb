75e9bbd06cbb8cd78a28abb3743187fbdb6e16b1
Dan <dthompson7@gmail.com>
2013-07-05 02:36:43 +0000

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 AcelixTest2::Application.routes.draw do
-  get "clubhome/index"
-  # You can have the root of your site routed with "root"
   root 'home#index'
+
+  get "clubhome/" => 'clubhome#index'
   get "home/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
